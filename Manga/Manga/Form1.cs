7a65263728c83e456df8df3d668d795df494a74c
Lorenzo <149068830+LorenzoDLaw@users.noreply.github.com>
2024-06-14 09:41:20 +0200

    File aggiornato
    Implementazione metodo per trovare il più costoso e il meno. Con un unica riga. (var manga = mangas.MinBy(x => x.Prezzo);)  questo metodo va a creare una variabile lista che inserisce al suoi interno i valori trovati

--- Manga/Manga/Form1.cs
+++ Manga/Manga/Form1.cs
@@ -26,38 +26,39 @@ namespace Manga
         }
         private void btnPiuCostoso_Click(object sender, EventArgs e)
         {
-            string titolo = "";
-            double prezzo = 0;
-            for (int i = 0; i < bindingSource1.Count; i++)
-            {
-                double prezzoCorrente = (double)((ModelloManga)bindingSource1.Current).Prezzo;
-                //double prezzoCorrente = Convert.ToDouble(TabellaManga.Rows[i].Cells[2].Value);
-                if (prezzo < prezzoCorrente)
-                {
-                    titolo = Convert.ToString(TabellaManga.Rows[i].Cells[1].Value);
-                    prezzo = prezzoCorrente;
-                }
-                  
-            }
-            MessageBox.Show($"Più costoso {titolo}","manga più costoso", MessageBoxButtons.OK);
+            //string titolo = "";
+            //double prezzo = 0;
+            //for (int i = 0; i < bindingSource1.Count; i++)
+            //{
+            //    //double prezzoCorrente = (double)((ModelloManga)bindingSource1.Current).Prezzo;
+            //    double prezzoCorrente = Convert.ToDouble(TabellaManga.Rows[i].Cells[3].Value);
+            //    if (prezzo < prezzoCorrente)
+            //    {
+            //        titolo = Convert.ToString(TabellaManga.Rows[i].Cells[1].Value);
+            //        prezzo = prezzoCorrente;
+            //    }
+
+            //}
+            var manga = mangas.MaxBy(x => x.Prezzo);
+            MessageBox.Show($"Più costoso {manga.Titolo}","manga più costoso", MessageBoxButtons.OK);
         }
 
         private void btnMenoCostoso_Click(object sender, EventArgs e)
         {
-            string titolo = "";
-            double prezzo = Convert.ToDouble(TabellaManga.Rows[0].Cells[2].Value);
-            for (int i = 0; i < bindingSource1.Count; i++)
-            {
-                double prezzoCorrente = (double)((ModelloManga)bindingSource1.Current).Prezzo;
-                //double prezzoCorrente = Convert.ToDouble(TabellaManga.Rows[i].Cells[2].Value);
-                if (prezzo > prezzoCorrente)
-                {
-                    titolo = Convert.ToString(TabellaManga.Rows[i].Cells[1].Value);
-                    prezzo = prezzoCorrente;
-                }
+            //string titolo = convert.tostring(tabellamanga.rows[0].cells[1].value);
+            //double prezzo = convert.todouble(tabellamanga.rows[0].cells[3].value);
+            //for (int i = 0; i < bindingsource1.count; i++)
+            //{
+            //    double prezzocorrente = convert.todouble(tabellamanga.rows[i].cells[3].value);
+            //    if (prezzo > prezzocorrente)
+            //    {
+            //        titolo = convert.tostring(tabellamanga.rows[i].cells[1].value);
+            //        prezzo = prezzocorrente;
+            //    }
 
-            }
-            MessageBox.Show($"Meno costoso {titolo}", "manga meno costoso", MessageBoxButtons.OK);
+            //}
+            var manga = mangas.MinBy(x => x.Prezzo);
+            MessageBox.Show($"Meno costoso {manga.Titolo}", "manga meno costoso", MessageBoxButtons.OK);
         }
     }
 }
